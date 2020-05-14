f38f7cc035aa7a60e3fdd91538299b6b943bef2f
Kyzent <Kyzent@hotmail.com>
2020-05-14 13:07:03 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demodatabase/MainActivity.java
@@ -0,0 +1,81 @@
+package com.myapplicationdev.android.demodatabase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnInsert;
+    Button btnGetTasks;
+    TextView tvResults;
+
+    ArrayList<Task> Tasks;
+    ListView lv;
+    ArrayAdapter aa;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnInsert = findViewById(R.id.btnInsert);
+        btnGetTasks = findViewById(R.id.btnGetTask);
+        tvResults = findViewById(R.id.textViewResults);
+
+        lv = (ListView) this.findViewById(R.id.lv);
+
+        DBHelper db = new DBHelper(MainActivity.this);
+        Tasks = db.getTasks();
+        db.close();
+
+        aa = new TaskAdapter(this, R.layout.row, Tasks);
+        lv.setAdapter(aa);
+
+        btnInsert.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                db.insertTask("Submit RJ", "25 Apr 2016");
+                db.close();
+            }
+        });
+
+        btnGetTasks.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                // Create the DBHelper object, passing in the
+                // activity's Context
+                DBHelper db = new DBHelper(MainActivity.this);
+
+                // Insert a task
+                ArrayList<String> data = db.getTaskContent();
+                Tasks = db.getTasks();
+                db.close();
+
+                String txt = "";
+                for (int i = 0; i < data.size(); i++) {
+                    Log.d("Database Content", i +". "+data.get(i));
+                    txt += i + ". " + data.get(i) + "\n";
+                }
+                aa.notifyDataSetChanged();
+
+                tvResults.setText(txt);
+            }
+        });
+
+
+    }
+}
---
+++ app/src/main/java/com/myapplicationdev/android/demodatabase/TaskAdapter.java
@@ -0,0 +1,57 @@
+package com.myapplicationdev.android.demodatabase;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
+public class TaskAdapter extends ArrayAdapter<Task> {
+
+    private ArrayList<Task> task;
+    private Context context;
+    private TextView tvID, tvDesc, tvDate;
+
+    public TaskAdapter(Context context, int resource, ArrayList<Task> objects){
+        super(context, resource, objects);
+        // Store the food that is passed to this adapter
+        task = objects;
+        // Store Context object as we would need to use it later
+        this.context = context;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        // The usual way to get the LayoutInflater object to
+        //  "inflate" the XML file into a View object
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        // "Inflate" the row.xml as the layout for the View object
+        View rowView = inflater.inflate(R.layout.row, parent, false);
+
+        // Get the TextView object
+        tvID = (TextView) rowView.findViewById(R.id.textViewID);
+        tvDesc = (TextView) rowView.findViewById(R.id.textViewDesc);
+        tvDate = (TextView) rowView.findViewById(R.id.textViewDate);
+
+        // The parameter "position" is the index of the
+        //  row ListView is requesting.
+        //  We get back the food at the same index.
+        Task currentTask = task.get(position);
+        // Set the TextView to show the food
+
+        tvID.setText(Integer.toString(currentTask.getId()));
+        tvDesc.setText(currentTask.getDescription());
+        tvDate.setText(currentTask.getDate());
+
+
+        // Return the nicely done up View to the ListView
+        return rowView;
+    }
+
+}
